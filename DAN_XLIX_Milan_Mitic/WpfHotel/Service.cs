ee0974280d9b93293c3b525fb8b0e6177b3f8329
Milan992 <mitic.gsm@gmail.com>
2020-08-04 12:05:18 +0200

    Implemented method IsOwner()

--- DAN_XLIX_Milan_Mitic/WpfHotel/Service.cs
+++ DAN_XLIX_Milan_Mitic/WpfHotel/Service.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,37 @@ namespace WpfHotel
 {
     class Service
     {
+        internal bool IsOwner(string userName, string password)
+        {
+            string[] userPass = new string[2];
+            int counter = 0;
+            using (StreamReader sr = new StreamReader("../../OwnerAccess.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    userPass[counter] = line;
+                    counter++;
+                }
+            }
+            if (userName == userPass[0] && password == userPass[1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal bool IsEmployee(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool IsManager(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
