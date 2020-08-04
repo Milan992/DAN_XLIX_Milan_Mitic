3696a7494939cc30fe71403cf655678191b1d498
Milan992 <mitic.gsm@gmail.com>
2020-08-04 11:43:40 +0200

    Created RelayCommand class

--- hotelDataBaseScript.sql
+++ hotelDataBaseScript.sql
@@ -4,14 +4,14 @@ GO
 USE Hotel;
 
 
-if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblAccount')
-drop table tblAccount;
-
 if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblManager')
 drop table tblManager;
 
 if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblEmployee')
 drop table tblEmployee;
+
+if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblAccount')
+drop table tblAccount;
 
 if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblEngagement')
 drop table tblEngagement;
@@ -26,10 +26,15 @@ drop table tblApplicationStatus;
 create table tblAccount(
 AccountID int identity(1,1) primary key,
 FullName varchar(30) not null,
-DateOfBirth date not null,
+DateOfBirth date CHECK ((DATEDIFF(year, getdate(), DateOfBirth)) < 16) not null,
 Email varchar(30),
 UserName varchar(30) not null unique,
 Pass varchar(30) not null)
+
+Create table tblEngagement (
+EngagementID int identity (1,1) primary key,
+EngagementName varchar(10)
+)
 
 create table tblEmployee(
 EmployeeID int identity(1,1) primary key,
@@ -38,11 +43,27 @@ HotelFloor int,
 Gender char(1) check(Gender in ('M', 'F', 'X')) not null,
 Cityzenship varchar(30),
 EngagementID int foreign key (EngagementID) references tblEngagement(EngagementID) not null,
-Salary int)
+Salary int
+)
 
 create table tblManager(
 ManagerID int identity(1,1) primary key,
 AccountID int foreign key (AccountID) references tblAccount(AccountID) not null,
 HotelFloor int,
 Experience int,
-QualificationsLevel varchar(3) check(QualificationsLevel in ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII')) not null)
+QualificationsLevel varchar(3) check(QualificationsLevel in ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII')) not null
+)
+
+Create table tblApplicationStatus (
+ApplicationStatusID int identity (1,1) primary key,
+ApplicationStatusName varchar(10)
+)
+
+Create table tblApplication (
+ApplicationID int identity (1,1) primary key,
+ApplicationStatusID int foreign key (ApplicationStatusID) references tblApplicationStatus(ApplicationStatusID) not null,
+StartDate date CHECK ((DATEDIFF(day, StartDate, getdate())) < 20) not null,
+EndDate date not null,
+Explanation varchar(30)
+)
+
